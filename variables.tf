5e6c011574bc761b664a4261d5e8d4662c58bbd6
LM Bibera <lyndon.bibera@headhuntr.io>
2021-04-17 13:32:56 +0800

    code cleanup

--- variables.tf
+++ variables.tf
@@ -13,23 +13,23 @@ variable "env" {
 variable "vpc_cidr" {
   description = "CIDR block for VPC"
   type        = string
-  default = "172.30.0.0/16"
+  default     = "172.30.0.0/16"
 }
 
 variable "vpc_public_cidrs" {
   description = "CIDR for public Subnet"
   type        = list(string)
-  default = ["172.30.0.0/20", "172.30.16.0/20", "172.30.32.0/20"]
+  default     = ["172.30.0.0/20", "172.30.16.0/20", "172.30.32.0/20"]
 }
 
 variable "vpc_private_cidrs" {
   description = "CIDR for private Subnet"
   type        = list(string)
-  default = ["172.30.128.0/20", "172.30.144.0/20", "172.30.160.0/20"]
+  default     = ["172.30.128.0/20", "172.30.144.0/20", "172.30.160.0/20"]
 }
 
 variable "eks_cluster_name" {
   description = "EKS cluster name"
   type        = string
-  default = "istio-experiment-eks"
+  default     = "istio-experiment-eks"
 }
