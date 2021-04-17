802f0d34dc8a751ce4a06b6e3021362198f47925
LM Bibera <lyndon.bibera@headhuntr.io>
2021-04-17 17:34:31 +0800

    updated cleanup

--- destroy.sh
+++ destroy.sh
@@ -3,5 +3,6 @@ terraform state rm helm_release.istio_ingress
 terraform state rm helm_release.istiod
 terraform state rm helm_release.istio_base
 terraform state rm kubernetes_namespace.istio_system
+terraform state rm 'module.eks_cluster.kubernetes_config_map.aws_auth[0]'
 
 terraform destroy --auto-approve
